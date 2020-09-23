c4d3d0f6470e236581b4eaa792550fc399449eb5
Gina Sprint <sprint@gonzaga.edu>
2020-09-23 11:20:31 -0700

    Initial Commit

---
+++ iOSBasicsFunS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSBasicsFunS2
+//
+//  Created by Gina Sprint on 9/23/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
