a4a5722ecaf8155763f745dc46fdf232bae33804
Gina Sprint <sprint@gonzaga.edu>
2020-09-23 11:32:12 -0700

    initial push in class

--- iOSBasicsFunS2/ViewController.swift
+++ iOSBasicsFunS2/ViewController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+// MARK: - Overview of an iOS Project
+// first, the folders are not folders, they are groups
+// changes to the groups do not change on disk
+// iOSBasicsFunS2
+// - AppDelegate.swift: middleman between your app and the OS
+// - SceneDelegate.swift: new in iOS13/Xcode 11... works with the AppDelegate to configure/manage scenes of your app
+// - ViewController.swift: this file!! part of MVC
+// - Main.storyboard: this is an archive of view objects created/edited in Interface Builder (IB)
+// - Assets.xcassets: collection of assets/resources for your app (e.g. images, sounds, etc.)
+// - LaunchScreen.storyboard: when the app launches there is a launch screen (AKA load screen AKA splash screen)
+// - Info.plist: "property list" a dictionary storing important info about your app
+// in Android, AndroidManifest.xml
+//
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
